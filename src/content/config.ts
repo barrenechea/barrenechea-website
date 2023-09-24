eb00eddcaa706f8e17b46129c1e8319e826afc27
Sebastian Barrenechea <sebastian@barrenechea.cl>
2023-09-24 10:34:12 -0300

    chore: setup content structure, cleanup

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,28 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const postCollection = defineCollection({
-  /* ... */
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    ogOverride: z.string().optional(),
+    description: z.string(),
+    pubDate: z.date(),
+    imgSrc: z.string(),
+    imgAlt: z.string(),
+    tags: z.array(z.string()),
+  }),
 });
+
 const projectCollection = defineCollection({
-  /* ... */
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    link: z.string().optional(),
+    imgSrc: z.string(),
+    imgAlt: z.string(),
+  }),
 });
 
 export const collections = {
