c5970a59d9e226a556660561495dd43dce7a788f
Sebastian Barrenechea <sebastian@barrenechea.cl>
2023-09-25 14:07:16 -0300

    fix: update rss implementation

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,25 +1,27 @@
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
 
-import { IMDX } from '@/types/imdx';
-import { AppConfig } from '@/utils/AppConfig';
+import { AppConfig } from "@/utils/AppConfig";
 
-const mdxResults = import.meta.glob('./**/*.mdx', { eager: true }) as unknown as IMDX[];
+const customData = "<language>en-us</language>";
 
-const mdx = Object.values(mdxResults);
+export async function GET(context: APIContext) {
+  const posts = await getCollection("posts");
+  const projects = await getCollection("projects");
+  const mdx = [...posts, ...projects];
 
-const customData = '<language>en-us</language>';
-
-export const get = () =>
-  rss({
+  return rss({
     title: AppConfig.siteName,
     description: AppConfig.description,
-    site: import.meta.env.SITE,
+    site: context.site as URL,
     items: mdx.map((post) => ({
-      link: post.url,
-      title: post.title,
-      description: post.description,
-      pubDate: post.pubDate,
+      link: `/${post.collection}/${post.slug}`,
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
       customData,
     })),
     customData,
   });
+}
