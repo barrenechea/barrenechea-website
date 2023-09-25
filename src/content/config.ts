45f38de3ef72b9f4903c6214dc67d74109f62019
Sebastian Barrenechea <sebastian@barrenechea.cl>
2023-09-25 09:35:34 -0300

    feat: switch to frontmatter for content metadata

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,26 +2,26 @@ import { defineCollection, z } from "astro:content";
 
 const postCollection = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     ogOverride: z.string().optional(),
     description: z.string(),
     pubDate: z.date(),
-    imgSrc: z.string(),
+    img: image(),
     imgAlt: z.string(),
-    tags: z.array(z.string()),
   }),
 });
 
 const projectCollection = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.date(),
     link: z.string().optional(),
-    imgSrc: z.string(),
+    img: image(),
     imgAlt: z.string(),
+    tags: z.array(z.string()),
   }),
 });
 
