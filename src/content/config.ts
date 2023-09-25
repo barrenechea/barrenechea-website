dd53cd9b6f41cd85a3ea88ce43bda26886111d6f
Sebastian Barrenechea <sebastian@barrenechea.cl>
2023-09-25 18:28:32 -0300

    feat: Content Collections (#3)
    * chore: reorg content location
    
    * chore: setup content structure, cleanup
    
    * chore: update deps
    
    * feat: switch to frontmatter for content metadata
    
    * fix: new props handling
    
    * feat: basic slug rendering
    
    * chore: renamed content to match existing routes
    
    * fix: update rss implementation
    
    * chore: remove astro-compress (not really needed)
    
    * chore: simplify typings, fixed build for posts
    
    * chore: cleanup
    
    * chore: refresh social icons

---
+++ src/content/config.ts
@@ -0,0 +1,31 @@
+import { defineCollection, z } from "astro:content";
+
+const postsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    ogOverride: z.string().optional(),
+    description: z.string(),
+    pubDate: z.date(),
+    img: image(),
+    imgAlt: z.string(),
+  }),
+});
+
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    link: z.string().optional(),
+    img: image(),
+    imgAlt: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  posts: postsCollection,
+  projects: projectsCollection,
+};
--- src/utils/Posts.ts
+++
@@ -1,5 +0,0 @@
-import type { IMDX } from '@/types/imdx';
-
-export const sortByDate = (posts: IMDX[]) => {
-  return posts.sort((a, b) => b.pubDate.getTime() - a.pubDate.valueOf());
-};
