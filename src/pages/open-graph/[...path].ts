52f0e61650ee72c71a3d1bf75a30b71716b72b56
Sebastian Barrenechea <sebastian@barrenechea.cl>
2023-11-20 15:40:41 -0300

    chore: update og paths

--- src/pages/open-graph/[...path].ts
+++ src/pages/open-graph/[...path].ts
@@ -8,39 +8,46 @@ const defaultOgImage = {
   },
 };
 
-const pages = await import.meta.glob("/src/content/**/*.mdx", { eager: true });
-pages["/default-og-image"] = defaultOgImage;
+const allPages = await import.meta.glob("/src/content/**/*.mdx", { eager: true });
+
+/** An object mapping file paths to file metadata. */
+const pages = Object.fromEntries(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Object.values(allPages).map(({ url, frontmatter }: any) => [url.split("content/")[1], { frontmatter }])
+);
+
+pages["default-og-image"] = defaultOgImage;
 
 export const { getStaticPaths, GET } = OGImageRoute({
   param: "path",
   pages,
-  getImageOptions: (path, page) => ({
-    title: page.frontmatter.title,
-    description: page.frontmatter.description,
-      logo: {
-        path: "./src/assets/og-image.png",
-        size: [200],
+  getImageOptions: (_, { frontmatter }: (typeof pages)[string]) => ({
+    title: frontmatter.title,
+    description: frontmatter.description,
+    logo: {
+      path: "./src/assets/og-image.png",
+      size: [200],
+    },
+    border: { color: [251, 191, 36], width: 20, side: "inline-start" },
+    bgGradient: [
+      [24, 24, 27],
+      [39, 39, 42],
+    ],
+    font: {
+      title: {
+        size: 50,
+        families: ["Work Sans"],
+        weight: "SemiBold",
       },
-      border: { color: [251, 191, 36], width: 20, side: "inline-start" },
-      bgGradient: [
-        [24, 24, 27],
-        [39, 39, 42],
-      ],
-      font: {
-        title: {
-          size: 50,
-          families: ["Work Sans"],
-          weight: "SemiBold",
-        },
-        description: {
-          size: 38,
-          families: ["Work Sans"],
-          weight: "Normal",
-        },
+      description: {
+        size: 38,
+        families: ["Work Sans"],
+        weight: "Normal",
       },
-      fonts: [
-        "./src/pages/open-graph/_fonts/work-sans/latin-400-normal.ttf",
-        "./src/pages/open-graph/_fonts/work-sans/latin-800-normal.ttf",
-      ],
+    },
+    fonts: [
+      "./src/pages/open-graph/_fonts/work-sans/latin-400-normal.ttf",
+      "./src/pages/open-graph/_fonts/work-sans/latin-800-normal.ttf",
+    ],
   }),
 });
