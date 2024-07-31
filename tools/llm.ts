2e6a78d39ccc3da122e8bc7183d457992c2d0336
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-07-31 19:48:44 -0400

    chore: update deps

--- tools/llm.ts
+++ tools/llm.ts
@@ -10,7 +10,7 @@ interface LlmPayload {
   temperature: number;
 }
 
-export const model = process.env.LLM_MODEL ?? 'gpt-4-turbo-preview';
+export const model = process.env.LLM_MODEL ?? 'gpt-4o';
 
 // requires to have OPENAI_API_KEY set in env
 // optionally, OPENAI_BASE_URL can be set to point to a different API endpoint
